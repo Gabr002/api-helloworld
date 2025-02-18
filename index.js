ee5fba48295a464c04dc04abe3fe6ae0d1db3698
Gabriel Dantas <98811096+Gabr002@users.noreply.github.com>
2025-02-18 12:36:18 -0300

    fix: routes for files externs

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const { apiSomar, apiSubtrair, apiMultiplicar, apiDividir } = require("./api/operacoes");
+const { apiSomar, 
+        apiSub, 
+        apiMult, 
+        apiDiv
+    } = require("./api/operacoes");
 const app = express();
 const port = 3000;
-app.use = (express.json());
+app.use(express.json());
 
 app.post('/somar', apiSomar);
-// app.post('/api/mul', apiSubtrair);
-// app.post('/api/sub', apiMultiplicar);
-// app.post('/api/div', apiDividir);
+
+app.post('/sub', apiSub);
+
+app.post('/mult', apiMult);
+
+app.post('/div', apiDiv);
 
 app.listen(port, () => {
     console.log('servidor rodando na porta ' + port)
