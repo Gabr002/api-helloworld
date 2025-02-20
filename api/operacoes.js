2e4472bf4414932da7f20c267b726d0db423925f
Gabriel Dantas <98811096+Gabr002@users.noreply.github.com>
2025-02-20 11:47:09 -0300

    fix: add tratament Erros

--- api/operacoes.js
+++ api/operacoes.js
@@ -1,31 +1,45 @@
 const { somar, mult, sub, div } = require("../services/service");
 
 function apiSomar(req, res){
-    console.log(req.body)
-    const { num1, num2 } = req.body;
-    const result = somar(num1, num2);
-    res.status(200).send({ result });
+    try {
+        const { num1, num2 } = req.body;
+        const result = somar(num1, num2);
+        res.status(200).send({ result });
+    } catch (e) {
+        res.status(500).send({ msg: e.message })
+    }
 };
 
 function apiSub(req, res){
-    console.log(req.body)
-    const { num1, num2 } = req.body;
-    const result = sub(num1, num2);
-    res.status(200).send({ result });
+    try {
+        const { num1, num2 } = req.body;
+        const result = sub(num1, num2);
+        res.status(200).send({ result });
+    } catch (e) {
+        res.status(500).send({ msg: e.message })
+    }
 };
 
 function apiMult(req, res){
-    console.log(req.body)
-    const { num1, num2 } = req.body;
-    const result = mult(num1, num2);
-    res.status(200).send({ result });
+    try {
+        const { num1, num2 } = req.body;
+        const result = mult(num1, num2);
+        res.status(200).send({ result });
+    } catch (e) {
+        res.status(500).send({ msg: e.message })
+    }
+
+    
 };
 
 function apiDiv(req, res){
-    console.log(req.body)
-    const { num1, num2 } = req.body;
-    const result = div(num1, num2);
-    res.status(200).send({ result });
+    try {
+        const { num1, num2 } = req.body;
+        const result = div(num1, num2);
+        res.status(200).send({ result });
+    } catch (e) {
+        res.status(500).send({ msg: e.message })
+    }
 };
 
 module.exports = { apiSomar, apiSub, apiMult, apiDiv }
